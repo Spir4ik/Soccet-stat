22a5b499332fe785db5394693e9d92b9d0eae541
Egor Furanin <sven2.cool@gmail.com>
2021-03-04 20:04:58 +0300

    Мелкие поправки

--- src/components/Async_calls/AsyncCallsMatches.js
+++ src/components/Async_calls/AsyncCallsMatches.js
@@ -3,7 +3,6 @@ export default (numberLeague, yearSeason, status, resource, matchOrTeam) => {
         url: `https://api.football-data.org/v2/${resource}/${numberLeague}/${matchOrTeam}?status=${status}&season=${yearSeason}`,
         method: 'GET',
         headers: {
-            'Access-Control-Allow-Origin': 'spir4ik.github.io',
             'X-Auth-Token': '31da4377f6bd472d89c5c79443bfb5db',
             'Content-type': 'application/json',
         }
